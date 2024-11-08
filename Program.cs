12baedbb869865c4be85f7e5cba15b6f6a48afd5
Allaf <allaflima@hotmail.com>
2024-11-07 22:54:38 -0300

    feat: Atualizar projeto para .NET 8 e implementar herança em classes de smartphones
    - Atualizado o Target Framework de net6.0 para net8.0 no arquivo .csproj.
    - Implementadas as classes Iphone e Nokia herdando de Smartphone.
    - Adicionado o método InstalarAplicativo nas classes Iphone e Nokia com sobrescrita.
    - Ajustado o construtor da classe Smartphone para receber os parâmetros modelo, IMEI, e memoria.
    - Implementados testes no Program.cs para verificar o funcionamento das classes Nokia e Iphone.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 123", "123456789", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("987654321", "Iphone 4", "987654321", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
