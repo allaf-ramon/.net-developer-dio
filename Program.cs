d4339bb7ecedc69ee42e6898a82248b0de91b8db
Allaf <allaflima@hotmail.com>
2024-11-22 13:28:20 -0300

    feat: Adicionar suporte para Swagger e endpoint de veículos
    - Integração do Swagger para documentação da API
    - Adição do VeiculoServico e endpoint de veículos relacionado
    - Atualização da rota do endpoint de login para seguir convenções REST
    - Adicionado pacote Swashbuckle.AspNetCore
    - Organização do código com comentários de região
    - Implementação de endpoint básico de criação de veículo

--- Program.cs
+++ Program.cs
@@ -1,23 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
 using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.ModelViews;
 using MinimalApi.Dominio.Servicos;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Database;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApiContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => "Hello World!");
+#region Home
+app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/Login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
     var admin = administradorServico.Login(loginDTO);
 
@@ -28,5 +38,24 @@ app.MapPost("/Login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
 
     return Results.BadRequest("Usuário ou senha inválidos!");
 });
+#endregion
 
+#region Veiculos
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = new Veiculo{
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+    };
+    veiculoServico.Incluir(veiculo);
+    
+    return Results.Created($"/veiculos/{veiculo.Id}", veiculo);
+});
+#endregion
+
+#region App
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
+#endregion
